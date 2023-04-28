8471dc176f28c0bcb13205162bf96cec18f0427d
Abhishek Chaudhari <achaudhari@dateconomy.io>
2023-04-28 12:08:25 +0530

    Project Setup

---
+++ src/main/java/com/vap/projectM/ProjectMApplication.java
@@ -0,0 +1,13 @@
+package com.vap.projectM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vap/projectM/ProjectMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vap.projectM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
