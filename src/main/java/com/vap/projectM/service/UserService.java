50621d3312a572575f61cf93ff2600359e1ff2c5
VaishnavAbanave <vabanave@dataeconomy.io>
2023-04-28 15:09:44 +0530

    Added UserService interface and UserServiceImpl class declare validateUser method

---
+++ src/main/java/com/vap/projectM/service/UserService.java
@@ -0,0 +1,9 @@
+package com.vap.projectM.service;
+
+import com.vap.projectM.entity.User;
+
+public interface UserService {
+
+    public Boolean validateUser(User user);
+
+}
---
+++ src/main/java/com/vap/projectM/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.vap.projectM.service.impl;
+
+import com.vap.projectM.entity.User;
+import com.vap.projectM.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public Boolean validateUser(User user) {
+        return null;
+    }
+}
